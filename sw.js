e9645c181f2774a11698660b631c09f89f1f01b8
milan <milan.duettra@gmail.com>
2026-04-09 19:00:26 +0200

    fixxed buga 	modified:   index.html 	modified:   manifest.json 	modified:   sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,23 @@
-const CACHE = 'quitly-v1';
-const ASSETS = ['/index.html'];
+const CACHE = 'quitly-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'logo.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
